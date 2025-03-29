e8971a334c61163a4df8f9c4acb55796139e8cb0
rasaku351 <jiiihhi351@gmail.com>
2025-03-29 14:24:02 +0900

    반복문

---
+++ Chapter1/src/hello/PrintData.java
@@ -0,0 +1,28 @@
+package hello;
+
+public class PrintData {
+
+	public static void main(String[] args) {
+		// 데이터 - 숫자(정수, 실수), 문자
+		// 숫자
+		System.out.println(100); //정수
+		System.out.println(3.3); //실수
+		
+		// 문자
+		System.out.println('A'); //한 개의 문자 - '' 사용
+		System.out.println('가');
+		System.out.println("apple"); //여러 개의 문자(문자열) - "" 사용
+		
+		// 연산
+		System.out.println(4 + 5);
+		System.out.println(4 - 5);
+		System.out.println(4 * 5);
+		System.out.println(4.0 / 5.0);
+		
+		// 불리언(Boolean) - 결과가 true/false로 되는 자료형
+		System.out.println(4 > 5); //false
+		System.out.println(true); //true
+		
+	}
+
+}
